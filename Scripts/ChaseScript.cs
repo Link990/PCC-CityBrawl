6d4042f6dc6dbaa7b380786ecf37fd3786102993
Link990 <48072305+Link990@users.noreply.github.com>
2019-03-20 19:33:50 -0300

    Add files via upload
    Adds a few general scripts, alongside the two new attack and chase scripts for the AI.

---
+++ Scripts/ChaseScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseScript : MonoBehaviour
+{
+
+    public CpuMovement movement;
+    public Rigidbody rb;
+    public Transform parent;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponentInParent<CpuMovement>();
+        rb = GetComponentInParent<Rigidbody>();
+        parent = GetComponentInParent<Transform>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("CompareTagChase OK");
+            enemy.transform.LookAt(movement.player);
+            movement.Chasing = true;
+        } else
+        {
+            movement.Chasing = false;
+        }
+    }
+}
---
+++ Scripts/DummyCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyCollision : MonoBehaviour {
+    public float health = 100;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "HitType1")
+        {
+            health -= 25;
+        }
+        if(collision.collider.tag == "HitType2")
+        {
+            health -= 50;
+        }
+    }
+
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
